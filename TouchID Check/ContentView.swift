03590f7baa03190c8cc237d009725c131959cb7e
Shota Shimazu <hornet.live.mf@gmail.com>
2021-11-10 01:39:56 +0900

    Initial Commit

---
+++ TouchID Check/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TouchID Check
+//
+//  Created by Shota Shimazu on 2021/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TouchID Check/TouchID_CheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchID_CheckApp.swift
+//  TouchID Check
+//
+//  Created by Shota Shimazu on 2021/11/10.
+//
+
+import SwiftUI
+
+@main
+struct TouchID_CheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
